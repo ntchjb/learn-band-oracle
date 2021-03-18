4c6f8de434bb2ee48c5cd3891851a064b07d695b
Nathachai Jaiboon <ntchjb@users.noreply.github.com>
2021-03-18 10:57:42 +0700

    Fix script

--- owasm/deploy.sh
+++ owasm/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-cd ./owasm && RUSTFLAGS='-C link-arg=-s' cargo build --release --target wasm32-unknown-unknown && cd ../
+RUSTFLAGS='-C link-arg=-s' cargo build --release --target wasm32-unknown-unknown
 
-bandd tx oracle create-oracle-script --name "Gold Price" --description "get gold prices in USD and average them" --script ./target/wasm32-unknown-unknown/release/learn_owasm.wasm --owner band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs --from devnet-admin --keyring-backend test --node "tcp://rpc-master.d3n.xyz:26657" --chain-id bandchain --schema "{multiplier:u64}/{price:u64}" --gas 1000000
+bandd tx oracle create-oracle-script --name "Gold Price" --description "get gold prices in USD and average them" --script ./target/wasm32-unknown-unknown/release/learn_owasm.wasm --owner band1m5lq9u533qaya4q3nfyl6ulzqkpkhge9q8tpzs --from requester --keyring-backend test --node "tcp://localhost:26657" --chain-id bandchain --schema "{multiplier:u64}/{price:u64}" --gas 1000000
