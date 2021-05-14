b478ac8f2e1de7ffe15241335795096c469a199e
Nathachai Jaiboon <ntchjb@users.noreply.github.com>
2021-05-14 15:23:06 +0700

    Use bash

--- deploy-datasrc-laozi.sh
+++ deploy-datasrc-laozi.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 OWNER=$OWNER
 FROM=$FROM
--- owasm/deploy-laozi.sh
+++ owasm/deploy-laozi.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 OWNER=$OWNER
 FROM=$FROM
@@ -8,4 +8,4 @@ CHAIN_ID=$CHAIN_ID
 
 RUSTFLAGS='-C link-arg=-s' cargo build --release --target wasm32-unknown-unknown
 
-bandd tx oracle create-oracle-script --name "Gold Price" --description "get gold prices in USD and average them" --script ./target/wasm32-unknown-unknown/release/learn_owasm.wasm --owner $OWNER  --from $FROM --keyring-backend $KEYRING --node "tcp://${NODE_IP}:26657" --chain-id $CHAIN_ID --schema "{multiplier:u64}/{price:u64}" --gas 1000000
+bandd tx oracle edit-oracle-script 1 --name "Gold Price" --description "get gold prices in USD and average them" --script ./target/wasm32-unknown-unknown/release/learn_owasm.wasm --owner $OWNER  --from $FROM --keyring-backend $KEYRING --node "tcp://${NODE_IP}:26657" --chain-id $CHAIN_ID --schema "{multiplier:u64}/{price:u64}" --gas 1000000
