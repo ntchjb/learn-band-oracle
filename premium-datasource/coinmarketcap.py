ccb4203c75aace35894e22708b19f24a5d03ee62
Nathachai Jaiboon <ntchjb@users.noreply.github.com>
2021-05-17 13:38:27 +0700

    Premium data source

---
+++ premium-datasource/coinmarketcap.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import requests
+import sys
+import os
+import typing
+
+URL = "https://asia-southeast2-price-caching.cloudfunctions.net/query-price"
+HEADERS = {"Content-Type": "application/json"}
+
+def set_header_from_env(headers: typing.Dict[str, str], key: str):
+    value = os.environ.get(key)
+    if value != None:
+        headers[key] = value
+
+def set_request_verification_headers(existingHeaders: typing.Dict[str, str]) -> typing.Dict[str, str]:
+    newHeaders = existingHeaders.copy()
+    set_header_from_env(newHeaders, "BAND_CHAIN_ID")
+    set_header_from_env(newHeaders, "BAND_VALIDATOR")
+    set_header_from_env(newHeaders, "BAND_REQUEST_ID")
+    set_header_from_env(newHeaders, "BAND_EXTERNAL_ID")
+    set_header_from_env(newHeaders, "BAND_REPORTER")
+    set_header_from_env(newHeaders, "BAND_SIGNATURE")
+    return newHeaders
+
+def main(symbols):
+    payload = {"source": "cmc", "symbols": symbols}
+    headers = set_request_verification_headers(HEADERS)
+    print(headers)
+    r = requests.post(URL, headers=headers, json=payload)
+    r.raise_for_status()
+    pxs = r.json()
+
+    if len(pxs) != len(symbols):
+        raise Exception("PXS_AND_SYMBOL_LEN_NOT_MATCH")
+
+    return ",".join(pxs)
+
+
+if __name__ == "__main__":
+    try:
+        print(main(sys.argv[1:]))
+    except Exception as e:
+        print(str(e), file=sys.stderr)
+        sys.exit(1)
