db791fb6ceb23dd35c42d55b1085c233edf0361b
Nathachai Jaiboon <ntchjb@users.noreply.github.com>
2021-05-17 12:15:29 +0700

    Fix commands

--- send-request-guanyu.sh
+++ send-request-guanyu.sh
@@ -9,5 +9,5 @@ ORACLE_SCRIPT_ID=$ORACLE_SCRIPT_ID
 MIN_COUNT=$MIN_COUNT
 ASK_COUNT=$ASK_COUNT
 
-bandcli tx oracle request $ORACLE_SCRIPT_ID $ASK_COUNT $MIN_COUNT -c "0000000000000064" -m from_bandd --from $FROM --chain-id $CHAIN_ID --gas 3000000 --node tcp://$NODE_IP:26657
+bandcli tx oracle request $ORACLE_SCRIPT_ID $ASK_COUNT $MIN_COUNT -c "0000000000000064" -m from_bandd --from $FROM --chain-id $CHAIN_ID --gas 3000000 --node tcp://$NODE_IP:26657 --keyring-backend $KEYRING
 
