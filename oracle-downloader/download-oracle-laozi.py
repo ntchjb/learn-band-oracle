d748553e40df9bfabaa2173f916b9ee3f5a31511
Nathachai Jaiboon <ntchjb@users.noreply.github.com>
2021-07-07 13:30:03 +0700

    Update oracle downloader for laozi

--- oracle-downloader/download-oracle-laozi.py
+++ oracle-downloader/download-oracle-laozi.py
@@ -1,17 +1,19 @@
 import requests
 import os
 import json
+import base64
 import errno
 
-COUNTS_URL = "https://guanyu-testnet3-query.bandchain.org/oracle/counts"
-DATASOURCE_BASE_URL = "https://guanyu-testnet3-query.bandchain.org/oracle/data_sources"
-ORACLE_SCRIPT_BASE_URL = "https://guanyu-testnet3-query.bandchain.org/oracle/oracle_scripts"
-DATA_BASE_URL = "https://guanyu-testnet3-query.bandchain.org/oracle/data"
+COUNTS_URL = "https://laozi-testnet2.bandchain.org/oracle/v1/counts"
+DATASOURCE_BASE_URL = "https://laozi-testnet2.bandchain.org/oracle/v1/data_sources"
+ORACLE_SCRIPT_BASE_URL = "https://laozi-testnet2.bandchain.org/oracle/v1/oracle_scripts"
+DATA_BASE_URL = "https://laozi-testnet2.bandchain.org/oracle/v1/data"
 
 def GET(url, height):
-  return requests.get(url + "?height=" + str(height), headers={'User-Agent': 'curl/7.64.1'}).json()
+  return requests.get(url, headers={'User-Agent': 'curl/7.64.1', 'x-cosmos-block-height': height}).json()
 def GET_FILE(url, height):
-  return requests.get(url + "?height=" + str(height), headers={'User-Agent': 'curl/7.64.1'}).content
+  result = requests.get(url, headers={'User-Agent': 'curl/7.64.1', 'x-cosmos-block-height': height}).json()
+  return base64.b64decode(result['data'])
 def WRITE_TO_FILE(filePath, msg):
   if not os.path.exists(os.path.dirname(filePath)):
     try:
@@ -41,23 +43,23 @@ def main():
   counts = GET(COUNTS_URL, blockHeight)
   print(counts)
   
-  dataSourceCount = int(counts["result"]["data_source_count"])
-  oracleScriptCount = int(counts["result"]["oracle_script_count"])
+  dataSourceCount = int(counts["data_source_count"])
+  oracleScriptCount = int(counts["oracle_script_count"])
 
   dataSources = []
   oracleScripts = []
   for i in range(1, dataSourceCount):
     dataSource = GET(DATASOURCE_BASE_URL + "/" + str(i), blockHeight)
     print(dataSource)
-    file = GET_FILE(DATA_BASE_URL + "/" + dataSource["result"]["filename"], blockHeight)
-    WRITE_TO_FILE(filesPath + "/" + dataSource["result"]["filename"], file)
-    dataSources.append(dataSource["result"])
+    file = GET_FILE(DATA_BASE_URL + "/" + dataSource["data_source"]["filename"], blockHeight)
+    WRITE_TO_FILE(filesPath + "/" + dataSource["data_source"]["filename"], file)
+    dataSources.append(dataSource["data_source"])
   for i in range(1, oracleScriptCount):
     oracleScript = GET(ORACLE_SCRIPT_BASE_URL + "/" + str(i), blockHeight)
     print(oracleScript)
-    file = GET_FILE(DATA_BASE_URL + "/" + oracleScript["result"]["filename"], blockHeight)
-    WRITE_TO_FILE(filesPath + "/" + oracleScript["result"]["filename"], file)
-    oracleScripts.append(oracleScript["result"])
+    file = GET_FILE(DATA_BASE_URL + "/" + oracleScript["oracle_script"]["filename"], blockHeight)
+    WRITE_TO_FILE(filesPath + "/" + oracleScript["oracle_script"]["filename"], file)
+    oracleScripts.append(oracleScript["oracle_script"])
 
   with open(genesisFilePathOriginal, encoding='utf-8') as fh:
     genesisState = json.load(fh)
